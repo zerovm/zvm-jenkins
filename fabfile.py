364f5fa9137042684462518c2b3faa4940978379
Lars Butler <Lars.Butler@gmail.com>
2013-12-31 16:22:11 +0100

    Cleaned up fabfile
    Removed unused imports, refactored functions, etc.

--- fabfile.py
+++ fabfile.py
@@ -1,16 +1,12 @@
-from fabric.api import local
-from fabric.api import sudo
 from fabric.api import cd
-from fabric.api import lcd
 from fabric.api import run
-from fabric.context_managers import shell_env
 
 
 WORKSPACE = '/home/ubuntu/zvm-toolchain'
-OTHER_LIBS = '%s/other_libs' % WORKSPACE
-ZEROVM_ROOT = '%s/zerovm' % OTHER_LIBS
-ZVM_PREFIX = '%s/zvm-root' % OTHER_LIBS
-ZRT_ROOT = '%s/zrt' % OTHER_LIBS
+HOME = '/home/ubuntu'
+ZEROVM_ROOT = '%s/zerovm' % HOME
+ZVM_PREFIX = '%s/zvm-root' % HOME
+ZRT_ROOT = '%s/zrt' % HOME
 VALZ = '%s/valz' % ZEROVM_ROOT
 PATH = '%s/bin:%s:$PATH' % (ZVM_PREFIX, VALZ)
 
@@ -26,9 +22,7 @@ ENV = dict(
     LD_LIBRARY_PATH=VALZ,
 )
 
-SCRIPT = """\
-git clone $GITURL/toolchain.git $WORKSPACE
-cd $WORKSPACE/SRC
+CLONE = """\
 git clone $GITURL/linux-headers-for-nacl.git
 git clone $GITURL/gcc.git
 git clone $GITURL/glibc.git
@@ -36,23 +30,58 @@ git clone $GITURL/newlib.git
 git clone $GITURL/binutils.git
 git clone $GITURL/zerovm.git $ZEROVM_ROOT
 git clone $GITURL/validator.git $ZEROVM_ROOT/valz
-git clone $GITURL/zrt.git $ZRT_ROOT
+git clone $GITURL/zrt.git $ZRT_ROOT\
+"""
 
-cd $VALZ
+VALIDATOR = """\
 make validator
 ln -s ./out/Release/libvalidator.so.0.9.0 libvalidator.so.0.9.0
 ln -s ./out/Release/libvalidator.so.0.9.0 libvalidator.so.0
-ln -s ./out/Release/libvalidator.so.0.9.0 libvalidator.so
+ln -s ./out/Release/libvalidator.so.0.9.0 libvalidator.so\
+"""
 
-cd $ZEROVM_ROOT
-make all install PREFIX=$ZVM_PREFIX
+ZEROVM = """\
+make all install PREFIX=$ZVM_PREFIX\
+"""
 
-cd $WORKSPACE
-make -j8
+BUILD = """\
+make -j8\
 """
 
 
-def test():
-    with shell_env(**ENV):
-        for cmd in SCRIPT.split('\n'):
-            run(cmd)
+def _run_with_env(env_dict, command):
+    environ = ';'.join(['export %s=%s' % (k, v) for k, v in env_dict.items()])
+    run('%s; %s' % (environ, command), pty=False)
+
+
+def _run_script(script):
+    for command in script.split('\n'):
+        _run_with_env(ENV, command)
+
+
+def run_all():
+    clone()
+    validator()
+    zerovm()
+    build()
+
+
+def clone():
+    _run_with_env(ENV, 'git clone $GITURL/toolchain.git $WORKSPACE')
+    with cd('%s/SRC' % WORKSPACE):
+        _run_script(CLONE)
+
+
+def validator():
+    with cd(VALZ):
+        _run_script(VALIDATOR)
+
+
+def zerovm():
+    with cd(ZEROVM_ROOT):
+        _run_script(ZEROVM)
+
+
+def build():
+    with cd(WORKSPACE):
+        _run_script(BUILD)
