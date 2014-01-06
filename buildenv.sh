0dfefc140c5e76d442e772ad2d955c624bcb11c6
Lars Butler <Lars.Butler@gmail.com>
2014-01-06 15:39:04 +0100

    Added build env bootstrap script
    This script creates an LXC instance for running a build, then attempts
    to get the IP address of machine and assigns it to the env var `IP`.

---
+++ buildenv.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -x
+set -e
+
+# TODO: Add a real check for these
+# The following env vars should be set:
+# - JOB_NAME (set by jenkins)
+# - BUILD_NUMBER (set by jenkins)
+# - GITURL
+# - RAWGITURL
+
+CURRENT_JOB_ID=$JOB_NAME-$BUILD_NUMBER
+IP=""
+
+get_ip () {
+    IP=`cat /var/lib/misc/dnsmasq.leases | grep "\s$CURRENT_JOB_ID\s" | awk '{print $3}'`
+}
+
+# clone an lxc container
+sudo lxc-clone -o $JOB_NAME -n $CURRENT_JOB_ID
+# start lxc, daemonized
+sudo lxc-start -n $CURRENT_JOB_ID -d
+
+# get the IP of the machine
+get_ip
+
+if [ -z "${IP}" ]; then
+    # it may take some time for the IP lease to take effect,
+    # so we can try multiple times before failing
+    times=(1 2 4 8 16)
+    for t in "${times[@]}"; do
+        echo "No IP yet assigned to LXC $CURRENT_JOB_ID -- waiting for $t seconds"
+        sleep $t
+        get_ip
+        if [ -z "${IP}" ]; then
+            # not yet defined
+            continue
+        else
+            break
+        fi
+    done
+fi
+
+if [ -z "${IP}" ]; then
+    echo "The LXC has did not get an IP"
+    exit 1
+fi
