bf996b9096348da5107352eb279f66a023ccf801
Lars Butler <Lars.Butler@gmail.com>
2014-01-06 15:50:11 +0100

    Added jenkins script for validator

---
+++ validator/jenkins.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -x
+set -e
+# run the build script remotely in the LXC
+# TODO: Add a real check for these
+# The following env vars should be set:
+# - IP
+# - GITURL
+# - BRANCH
+# - RAWGITURL
+
+
+echo "Deploying build script..."
+ssh ubuntu@$IP -oStrictHostKeyChecking=no "wget $RAWGITURL/zvm-jenkins/master/validator/build.sh"
+ssh ubuntu@$IP -oStrictHostKeyChecking=no "chmod +x ./build.sh"
+echo "Running build script..."
+ssh ubuntu@$IP -oStrictHostKeyChecking=no "./build.sh $GITURL $BRANCH"
+
+echo "Deploying packaging script..."
+ssh ubuntu@$IP -oStrictHostKeyChecking=no "wget $RAWGITURL/zvm-jenkins/master/validator/package.sh"
+ssh ubuntu@$IP -oStrictHostKeyChecking=no "chmod +x ./package.sh"
+echo "Creating packages..."
+ssh ubuntu@$IP -oStrictHostKeyChecking=no "./package.sh"
