4ed12f4aff4efb9b54508053864a926bf8860adb
Lars Butler <Lars.Butler@gmail.com>
2014-01-08 16:37:23 +0100

    Added package dep install step for zerovm build

--- zerovm/jenkins.sh
+++ zerovm/jenkins.sh
@@ -11,6 +11,14 @@ set -e
 # - LOCAL_PKG_DIR
 
 
+echo "Fetching package dependencies, publishing to local pkg repo..."
+# TODO: Revise this--don't copy all packages, just the ones we need
+# NOTE: The package file name can change from version to version
+scp -oStrictHostKeyChecking=no $LOCAL_PKG_DIR/*.deb ubuntu@$IP:$REMOTE_PKG_REPO_DIR
+ssh ubuntu@$IP -oStrictHostKeyChecking=no "./update-mydebs.sh $REMOTE_PKG_REPO_DIR"
+ssh ubuntu@$IP -oStrictHostKeyChecking=no "sudo apt-get update"
+ssh ubuntu@$IP -oStrictHostKeyChecking=no "sudo apt-get install --yes --force-yes libvalidator0"
+
 echo "Deploying build script..."
 ssh ubuntu@$IP -oStrictHostKeyChecking=no "wget $RAWGITURL/zvm-jenkins/master/zerovm/build.sh"
 ssh ubuntu@$IP -oStrictHostKeyChecking=no "chmod +x ./build.sh"
@@ -25,5 +33,5 @@ ssh ubuntu@$IP -oStrictHostKeyChecking=no "./package.sh"
 
 # Copy the built package to the host machine,
 # so that other build jobs can use it.
-echo "Copying built deb package..."
-scp ubuntu@$IP:/home/ubuntu/*.deb $LOCAL_PKG_DIR
+echo "Saving the built deb package..."
+scp -oStrictHostKeyChecking=no ubuntu@$IP:/home/ubuntu/*.deb $LOCAL_PKG_DIR
