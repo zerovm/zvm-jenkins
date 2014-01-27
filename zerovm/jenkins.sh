595b3d38aacadd9ad64d11b5908ca8268bf3a187
Lars Butler <Lars.Butler@gmail.com>
2014-01-27 16:12:30 +0100

    Fixed some wget calls for zerovm build

--- zerovm/jenkins.sh
+++ zerovm/jenkins.sh
@@ -20,13 +20,13 @@ ssh ubuntu@$IP -oStrictHostKeyChecking=no "mkdir $REMOTE_PKG_REPO_DIR"
 scp -oStrictHostKeyChecking=no $LOCAL_PKG_DIR/*.deb ubuntu@$IP:$REMOTE_PKG_REPO_DIR
 
 echo "Deploying build script..."
-ssh ubuntu@$IP -oStrictHostKeyChecking=no "wget $RAWGITURL/zvm-jenkins/master/zerovm/build.sh"
+ssh ubuntu@$IP -oStrictHostKeyChecking=no "wget --no-check-certificate $RAWGITURL/zvm-jenkins/master/zerovm/build.sh"
 ssh ubuntu@$IP -oStrictHostKeyChecking=no "chmod +x ./build.sh"
 echo "Running build script..."
 ssh ubuntu@$IP -oStrictHostKeyChecking=no "./build.sh $GITURL $BRANCH $REMOTE_PKG_REPO_DIR"
 
 echo "Deploying packaging script..."
-ssh ubuntu@$IP -oStrictHostKeyChecking=no "wget $RAWGITURL/zvm-jenkins/master/zerovm/package.sh"
+ssh ubuntu@$IP -oStrictHostKeyChecking=no "wget --no-check-certificate $RAWGITURL/zvm-jenkins/master/zerovm/package.sh"
 ssh ubuntu@$IP -oStrictHostKeyChecking=no "chmod +x ./package.sh"
 echo "Creating packages..."
 ssh ubuntu@$IP -oStrictHostKeyChecking=no "./package.sh"
