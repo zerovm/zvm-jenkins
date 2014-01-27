6e00784219075f358db95fca7c5b3c06e5347034
Lars Butler <Lars.Butler@gmail.com>
2014-01-27 15:27:51 +0100

    Added `make` to validator installed deps

--- validator/build.sh
+++ validator/build.sh
@@ -6,8 +6,9 @@ export WORKSPACE=$HOME/validator
 export GITURL=$1
 export BRANCH=$2
 
+DEPS="git make"
 sudo apt-get update
-sudo apt-get install --yes --force-yes git
+sudo apt-get install --yes --force-yes $DEPS
 
 git clone -b $BRANCH $GITURL/validator.git $WORKSPACE
 
