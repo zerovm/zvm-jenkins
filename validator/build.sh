a4bfaeda2f49194ec5a5c7c20ebd9a5e1abc5c0f
Lars Butler <Lars.Butler@gmail.com>
2014-01-27 15:22:49 +0100

    Added git installation to validator build script

--- validator/build.sh
+++ validator/build.sh
@@ -6,6 +6,9 @@ export WORKSPACE=$HOME/validator
 export GITURL=$1
 export BRANCH=$2
 
+sudo apt-get update
+sudo apt-get install --yes --force-yes git
+
 git clone -b $BRANCH $GITURL/validator.git $WORKSPACE
 
 cd $WORKSPACE
