41ae15b5161efeb04f016e16ffbfdd285f26aa07
Lars Butler <Lars.Butler@gmail.com>
2014-02-20 13:59:15 +0000

    validator: Add jenkins.sh script for non-nightlies
    This script does everything the nightly build does, except for
    publishing packages. It builds the packages, but does not attempt to
    publish them.

---
+++ validator/jenkins.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -x
+set -e
+# run the build script remotely in the LXC
+# TODO: Add a real check for these
+# The following env vars should be set:
+# - IP
+# - GITURL
+# - BRANCH
+# - RAWGITURL
+# - CI_NAME
+# - CI_EMAIL
+
+echo "Installing wget..."
+ssh ubuntu@$IP -oStrictHostKeyChecking=no "sudo apt-get install --yes --force-yes wget"
+echo "Deploying build script..."
+ssh ubuntu@$IP -oStrictHostKeyChecking=no "wget --no-check-certificate $RAWGITURL/zvm-jenkins/master/validator/build.sh"
+ssh ubuntu@$IP -oStrictHostKeyChecking=no "chmod +x ./build.sh"
+echo "Running build script..."
+ssh ubuntu@$IP -oStrictHostKeyChecking=no "./build.sh $GITURL $BRANCH"
+
+echo "Deploying packaging scripts..."
+ssh ubuntu@$IP -oStrictHostKeyChecking=no "wget --no-check-certificate $RAWGITURL/zvm-jenkins/master/validator/package.sh"
+ssh ubuntu@$IP -oStrictHostKeyChecking=no "wget --no-check-certificate $RAWGITURL/zvm-jenkins/master/packager.py"
+ssh ubuntu@$IP -oStrictHostKeyChecking=no "chmod +x ./package.sh"
+echo "Creating packages..."
+ssh ubuntu@$IP -oStrictHostKeyChecking=no "./package.sh \"$CI_NAME\" \"$CI_EMAIL\""
+
+# Copy the built package to the host machine,
+# so that other build jobs can use it.
+# echo "Copying built deb package..."
+# scp ubuntu@$IP:/home/ubuntu/*.deb $LOCAL_PKG_DIR
