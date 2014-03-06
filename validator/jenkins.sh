757610dccdb51f25a67d8adec94f15ddfca21ed3
Lars Butler <Lars.Butler@gmail.com>
2014-03-06 16:24:13 +0000

    validator/jenkins.sh: clean up comments

--- validator/jenkins.sh
+++ validator/jenkins.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 set -x
 set -e
-# run the build script remotely in the LXC
-# TODO: Add a real check for these
 # The following env vars should be set:
 # - GITURL
 # - BRANCH
 # - RAWGITURL
 # - CI_NAME
 # - CI_EMAIL
+# - PPA (optional)
 
 echo "Installing wget..."
 lxc_run sudo apt-get install --yes --force-yes wget
