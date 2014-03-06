1be98590da532fa3373d767f7a94d7a06dfbd75f
Lars Butler <Lars.Butler@gmail.com>
2014-03-06 16:35:59 +0000

    jenkins.sh: generic jenkins script
    For running everything. There are still project-specific scripts for the
    individual builds, but this removes a lot of duplication.

--- jenkins.sh
+++ jenkins.sh
@@ -9,15 +9,17 @@ set -e
 # - CI_EMAIL
 # - PPA (optional)
 
+source buildenv.sh
+
 echo "Installing wget..."
 lxc_run sudo apt-get install --yes --force-yes wget
 echo "Deploying build script..."
-lxc_run wget --no-check-certificate $RAWGITURL/zvm-jenkins/master/validator/build.sh
+lxc_run wget --no-check-certificate $RAWGITURL/zvm-jenkins/master/$JOB_NAME/build.sh
 echo "Running build script..."
 lxc_run sh build.sh $GITURL $BRANCH
 
 echo "Deploying packaging scripts..."
-lxc_run wget --no-check-certificate $RAWGITURL/zvm-jenkins/master/validator/package.sh
+lxc_run wget --no-check-certificate $RAWGITURL/zvm-jenkins/master/$JOB_NAME/package.sh
 lxc_run wget --no-check-certificate $RAWGITURL/zvm-jenkins/master/packager.py
 echo "Creating packages..."
 if [ -n "${PPA}" ]; then
