50002c65715b03b6aed7d69c8dadc51838c08ec2
Lars Butler <Lars.Butler@gmail.com>
2014-03-07 13:44:31 +0000

    toolchain/build.sh: make build process current
    This update to the build script follows the current build process
    defined in the toolchain README.
    
    One of the major improvements here is the ability to install zerovm and
    zvm-validator from packages from the `latest` repo.

--- toolchain/build.sh
+++ toolchain/build.sh
@@ -1,45 +1,41 @@
 #!/bin/bash
-# Jenkins build/test script for the zvm-toolchain
-# See $GITURL/toolchain
 set -x
 set -e
 
-export WORKSPACE=$HOME/zvm-toolchain  # Root working directory
-export GITURL=$1                      # example: "$GITURL"
-export TOOLCHAIN_BRANCH=$2            # branch of the toolchain to build
-export REMOTE_PKG_REPO_DIR=$3
+export WORKSPACE=$HOME/toolchain
+export GITURL=$1
+export BRANCH=$2
 
-sudo apt-get update
-sudo apt-get install --yes --force-yes dpkg-dev
+export ZRT_ROOT=$HOME/zrt
+export ZVM_PREFIX=$HOME/zvm-root
+export LD_LIBRARY_PATH=/usr/lib64
+export CPATH=/usr/x86_64-nacl/include
 
-echo "deb file:$REMOTE_PKG_REPO_DIR ./" | sudo tee -a /etc/apt/sources.list
-cd $REMOTE_PKG_REPO_DIR
-dpkg-scanpackages . /dev/null | gzip -9c > Packages.gz
-sudo apt-get update
-# We need to install this first before adding the ZVM repo,
-# so that we can install the local/latest version
-sudo apt-get install --yes --force-yes zerovm-zmq zerovm-zmq-dev
-sudo su -c 'echo "deb [ arch=amd64 ] http://zvm.rackspace.com/v1/repo/ubuntu/ precise main" > /etc/apt/sources.list.d/zerovm-precise.list'
-wget -O- https://zvm.rackspace.com/v1/repo/ubuntu/zerovm.pkg.key | sudo apt-key add -
-sudo apt-get update
-
-# install package deps
 DEPS="git libc6-dev-i386 libglib2.0-dev pkg-config build-essential automake"
 DEPS="$DEPS autoconf libtool g++-multilib texinfo flex bison groff gperf"
-DEPS="$DEPS texinfo subversion libzmq3 libzmq3-dev"
+DEPS="$DEPS texinfo subversion libzmq3 libzmq3-dev zerovm-zmq zerovm-zmq-dev"
+
+sudo apt-get update
+sudo apt-get install --yes --force-yes python-software-properties
+sudo add-apt-repository ppa:zerovm-ci/zerovm-latest
+sudo apt-get update
 sudo apt-get install --yes --force-yes $DEPS
 
-# we need zrt to build the toolchain
-export ZRT_ROOT=$HOME/zrt
+# we need zrt to build the toolchain as well:
 git clone $GITURL/zrt.git $ZRT_ROOT
-# Pull the main source repo:
-git clone -b $TOOLCHAIN_BRANCH $GITURL/toolchain.git $WORKSPACE
+
+git clone $GITURL/toolchain.git $WORKSPACE
+
 cd $WORKSPACE/SRC
 git clone $GITURL/linux-headers-for-nacl.git
 git clone $GITURL/gcc.git
 git clone $GITURL/glibc.git
 git clone $GITURL/newlib.git
 git clone $GITURL/binutils.git
+
 cd $WORKSPACE
-echo "Building toolchain..."
-make -j8
+git fetch origin +refs/pull/*:refs/remotes/origin/pr/*
+git checkout $BRANCH
+
+echo "Building toolchain. This could take a while..."
+make -j8 ZEROVM=`which zerovm`
