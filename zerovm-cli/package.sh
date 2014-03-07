2effb4c4dbe27eb3619f393264907c800043f96c
Lars Butler <Lars.Butler@gmail.com>
2014-03-07 17:28:45 +0000

    zerovm-cli/package.sh: install missing deb pkgs
    devscripts and debhelper were missing; we need these to roll packages
    (dch, debuild, etc.).

--- zerovm-cli/package.sh
+++ zerovm-cli/package.sh
@@ -2,7 +2,6 @@
 set -x
 set -e
 # Run ./build.sh first
-# Requires `sudo apt-get install devscripts debhelper`
 
 CI_NAME=$1
 CI_EMAIL=$2
@@ -13,7 +12,7 @@ export WORKSPACE=$HOME/zerovm-cli
 cd $WORKSPACE
 
 # packager.py needs this for parsing the changelog
-sudo apt-get install --yes --force-yes python-debian
+sudo apt-get install --yes --force-yes python-debian devscripts debhelper
 # Clean any test/coverage artifacts from the clone.
 # We need a pristine environment for packaging.
 # Things like .tox/ .coverage, etc. will make debuild unhappy
