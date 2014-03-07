25d751111ff3e5291efb18f307985ec9f0991df0
Lars Butler <Lars.Butler@gmail.com>
2014-03-07 15:14:00 +0000

    toolchain/build.sh: clean up post-build artifacts
    We need to clean out some temp files in order to follow up with
    packaging. We don't want the build artifacts to be included in the
    source tarball.

--- toolchain/build.sh
+++ toolchain/build.sh
@@ -11,6 +11,9 @@ export ZVM_PREFIX=$HOME/zvm-root
 export LD_LIBRARY_PATH=/usr/lib64
 export CPATH=/usr/x86_64-nacl/include
 
+# Toolchain sublibs
+TC_LIBS="linux-headers-for-nacl gcc glibc newlib binutils"
+
 DEPS="git libc6-dev-i386 libglib2.0-dev pkg-config build-essential automake"
 DEPS="$DEPS autoconf libtool g++-multilib texinfo flex bison groff gperf"
 DEPS="$DEPS texinfo subversion libzmq3 libzmq3-dev zerovm-zmq zerovm-zmq-dev"
@@ -27,11 +30,9 @@ git clone $GITURL/zrt.git $ZRT_ROOT
 git clone $GITURL/toolchain.git $WORKSPACE
 
 cd $WORKSPACE/SRC
-git clone $GITURL/linux-headers-for-nacl.git
-git clone $GITURL/gcc.git
-git clone $GITURL/glibc.git
-git clone $GITURL/newlib.git
-git clone $GITURL/binutils.git
+for lib in $TC_LIBS; do
+    git clone $GITURL/$lib.git
+done
 
 cd $WORKSPACE
 git fetch origin +refs/pull/*:refs/remotes/origin/pr/*
@@ -39,3 +40,10 @@ git checkout $BRANCH
 
 echo "Building toolchain. This could take a while..."
 make -j8 ZEROVM=`which zerovm`
+
+# Clean build artifacts before we continue to packaging:
+for lib in $TC_LIBS; do
+    cd $WORKSPACE/SRC/$lib
+    git reset --hard
+    git clean -fd
+done
