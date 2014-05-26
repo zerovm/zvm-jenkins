4b824a411a0aba3db24e3f21b1340a6c71e1395b
Lars Butler <Lars.Butler@gmail.com>
2014-05-26 13:06:24 +0200

    zpm: clean up for packaging integration

--- zpm/build.sh
+++ zpm/build.sh
@@ -6,13 +6,7 @@ export WORKSPACE=$HOME/zpm
 export GITURL=$1
 export BRANCH=$2
 
-sudo apt-get update
-sudo apt-get install --yes --force-yes python-pip
-sudo pip install tox
-
-git clone $GITURL/zpm.git
+git clone $GITURL/zpm.git $WORKSPACE
 cd $WORKSPACE
 git fetch origin +refs/pull/*:refs/remotes/origin/pr/*
 git checkout $BRANCH
-
-tox
